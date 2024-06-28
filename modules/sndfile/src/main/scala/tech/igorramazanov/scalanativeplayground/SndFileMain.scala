6091020b5c113f692f2b7dcc8256bbfcc41bdcac
Igor Ramazanov <igor.ramazanov@magine.com>
2024-06-28 20:22:19 +0000

    Bump nixpkgs, import alias for `sndfile`

--- modules/sndfile/src/main/scala/tech/igorramazanov/scalanativeplayground/SndFileMain.scala
+++ modules/sndfile/src/main/scala/tech/igorramazanov/scalanativeplayground/SndFileMain.scala
@@ -6,18 +6,16 @@ import scala.scalanative.unsafe.Ptr
 import scala.scalanative.unsafe.Zone
 import scala.scalanative.unsafe.fromCString
 import scala.scalanative.unsafe.toCString
-import tech.igorramazanov.scalanativeplayground.sndfile.SF_INFO
+import tech.igorramazanov.scalanativeplayground.sndfile as s
 import tech.igorramazanov.scalanativeplayground.sndfile.SF_INFO.*
-import tech.igorramazanov.scalanativeplayground.sndfile.SNDFILE
 
 object SndFileMain:
   def main(args: Array[String]): Unit = Zone(sndfileExample(args.head))
 
   def sndfileExample(file_path: String)(using Zone): Unit =
-    val sf_info: Ptr[SF_INFO] = sndfile.SF_INFO()
+    val sf_info: Ptr[s.SF_INFO] = s.SF_INFO()
     val path: Ptr[Byte] = toCString(file_path, StandardCharsets.UTF_8)
-    val file: Ptr[SNDFILE] = sndfile
-      .sf_open(path, sndfile.SFM_READ.toInt, sf_info)
+    val file: Ptr[s.SNDFILE] = sndfile.sf_open(path, s.SFM_READ.toInt, sf_info)
 
     println(s"Channels: ${(!sf_info).channels}")
     println(s"Format: ${(!sf_info).format}")
@@ -26,22 +24,22 @@ object SndFileMain:
     println(s"Sections: ${(!sf_info).sections}")
     println(s"Seekable: ${(!sf_info).seekable}")
 
-    read(file, sndfile.SF_STR_ALBUM, "Album")
-    read(file, sndfile.SF_STR_ARTIST, "Artist")
-    read(file, sndfile.SF_STR_COMMENT, "Comment")
-    read(file, sndfile.SF_STR_COPYRIGHT, "Copyright")
-    read(file, sndfile.SF_STR_DATE, "Date")
-    read(file, sndfile.SF_STR_GENRE, "Genre")
-    read(file, sndfile.SF_STR_LICENSE, "License")
-    read(file, sndfile.SF_STR_SOFTWARE, "Software")
-    read(file, sndfile.SF_STR_TITLE, "Title")
-    read(file, sndfile.SF_STR_TRACKNUMBER, "Tracknumber")
+    read(file, s.SF_STR_ALBUM, "Album")
+    read(file, s.SF_STR_ARTIST, "Artist")
+    read(file, s.SF_STR_COMMENT, "Comment")
+    read(file, s.SF_STR_COPYRIGHT, "Copyright")
+    read(file, s.SF_STR_DATE, "Date")
+    read(file, s.SF_STR_GENRE, "Genre")
+    read(file, s.SF_STR_LICENSE, "License")
+    read(file, s.SF_STR_SOFTWARE, "Software")
+    read(file, s.SF_STR_TITLE, "Title")
+    read(file, s.SF_STR_TRACKNUMBER, "Tracknumber")
 
-    val result = sndfile.sf_close(file)
+    val result = s.sf_close(file)
 
     if result != 0 then println("Failed to close the file: ${file_path}")
 
-  def read(file: Ptr[SNDFILE], str_type: CUnsignedInt, str_name: String): Unit =
-    val c_string = sndfile.sf_get_string(file, str_type.toInt)
+  def read(file: Ptr[s.SNDFILE], str_type: CUnsignedInt, str_name: String): Unit =
+    val c_string = s.sf_get_string(file, str_type.toInt)
     val str_value = fromCString(c_string, StandardCharsets.UTF_8)
     println(s"$str_name: $str_value")
